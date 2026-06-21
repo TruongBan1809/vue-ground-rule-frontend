2053a0c62ab30e844a19c8b6cd2aaa64d3c79b04
TruongBan1809 <longdinhnguyen393@gmail.com>
2026-06-21 12:15:34 +0700

    The first commit for vue ground rule frontend project

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Ứng dụng React của tôi đã chạy thành công trong Docker!</h1>
+  </React.StrictMode>
+)
